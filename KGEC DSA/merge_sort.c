481186bebb186700183532cf8e3a4c3dc1d9b2d1
Aritra Roy Choudhury <arc.nbg11@gmail.com>
2026-01-17 00:05:11 +0530

    Add files via upload

--- KGEC DSA/merge_sort.c
+++ KGEC DSA/merge_sort.c
@@ -1,82 +1,54 @@
 #include <stdio.h>
 
-// Function to merge two sorted subarrays
-void merge(int arr[], int left, int mid, int right) {
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    
-    // Create temporary arrays
+void merge(int arr[], int l, int m, int r)
+{
+    int n1 = m - l + 1;
+    int n2 = r - m;
     int L[n1], R[n2];
-    
-    // Copy data to temporary arrays
-    for (i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-        
-    // Merge the temporary arrays back into arr
-    i = 0;  // Initial index of first subarray
-    j = 0;  // Initial index of second subarray
-    k = left;  // Initial index of merged subarray
-    
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
-            i++;
-        } else {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+            arr[k++] = L[i++];
+        else
+            arr[k++] = R[j++];
     }
-    
-    // Copy remaining elements of L[] if any
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-    
-    // Copy remaining elements of R[] if any
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
+    while (i < n1)
+        arr[k++] = L[i++];
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = (l + r) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
 }
 
-// Main merge sort function
-void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
-        
-        // Sort first and second halves
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        
-        // Merge the sorted halves
-        merge(arr, left, mid, right);
-    }
-}
-
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++)
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
 
-int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int arr_size = sizeof(arr) / sizeof(arr[0]);
-    
+int main()
+{
+    int arr[] = {9, 4, 7, 2, 8, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
     printf("Original array: ");
-    printArray(arr, arr_size);
-    
-    mergeSort(arr, 0, arr_size - 1);
-    
-    printf("Sorted array: ");
-    printArray(arr, arr_size);
-    
+    printArray(arr, n);
+    mergeSort(arr, 0, n - 1);
+    printf("Sorted array (Merge Sort): ");
+    printArray(arr, n);
     return 0;
 }
