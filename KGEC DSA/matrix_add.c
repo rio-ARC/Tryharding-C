0b6e061da3003b6855c1674ede0b2d8dcb32f1eb
Aritra Roy Choudhury <arc.nbg11@gmail.com>
2026-01-31 19:54:08 +0530

    Add files via upload

---
+++ KGEC DSA/matrix_add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int r,c;
+    printf("Enter rows and columns of matrix: ");
+    scanf("%d %d", &r, &c);
+    int a[r][c], b[r][c], s[r][c];
+    printf("Enter elements of matrix A:\n");
+    for(int i=0; i<r; i++) for(int j=0; j<c; j++) scanf("%d ", &a[i][j]);
+    printf("Enter elements of matrix B:\n");
+    for(int i=0; i<r; i++) for(int j=0; j<c; j++) scanf("%d ", &b[i][j]);
+    printf("Sum of matrices A and B:\n");
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            s[i][j] = a[i][j] + b[i][j];
+            printf("%d ", s[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
